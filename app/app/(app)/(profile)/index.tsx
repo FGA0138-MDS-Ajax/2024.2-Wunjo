1d24ced20567b39138789f8f666153ed3d50ff9d
Yan Matheus <yanmathues0812@gmail.com>
2025-01-26 17:08:49 -0300

    ajuste na gamificacao

--- app/app/(app)/(profile)/index.tsx
+++ app/app/(app)/(profile)/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Switch, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, TextInput, Switch, StyleSheet, TouchableOpacity, Modal, Button } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import { Button as ButtonModal, ButtonText } from "@/components/ui/button";
 import { Itim_400Regular } from '@expo-google-fonts/itim';
 import { ScrollView } from 'react-native';
 
@@ -10,108 +11,151 @@ const PerfilScreen = () => {
   const [email, setEmail] = useState('renanv.guedes@aluno.unb.br');
   const [matricula, setMatricula] = useState('21012345');
   const [participarRanking, setParticiparRanking] = useState(true);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [newSwitchValue, setNewSwitchValue] = useState(false); 
+
+  const handleSwitchChange = (value: boolean) => {
+    if (!value) {
+      setNewSwitchValue(value);
+      setModalVisible(true); 
+    } else {
+      setParticiparRanking(true);
+    }
+  };
+
+  const confirmSwitchChange = () => {
+    setParticiparRanking(false);
+    setModalVisible(false);
+  };
+
+  const cancelSwitchChange = () => {
+    setModalVisible(false);
+    setNewSwitchValue(true); 
+  };
 
   return (
     <ScrollView>
-    <View style={styles.container}>
-      {/* Header */}
-      <View style={styles.header}>
-        <Ionicons name="person-circle" size={80} color="white" />
-        <Text style={styles.headerTitle}>Título</Text>
-      </View>
+      <View style={styles.container}>
 
-      {/* Informações Pessoais */}
-      <View style={styles.section}>
-        <Text style={styles.sectionTitle}>Informações Pessoais</Text>
+        {/* Header */}
+        <View style={styles.header}>
+          <Ionicons name="person-circle" size={80} color="white" />
+          <Text style={styles.headerTitle}>Título</Text>
+        </View>
 
-        <Text style={styles.sectionSubTitle}>Nome</Text>
-        <TextInput
-          style={styles.input}
-          value={nome}
-          onChangeText={setNome}
-          placeholder="Nome"
-        />
+        {/* Informações Pessoais */}
+        <View style={styles.section}>
+          <Text style={styles.sectionTitle}>Informações Pessoais</Text>
 
-        <Text style={styles.sectionSubTitle}>Curso</Text>
-        <TextInput
-          style={styles.input}
-          value={curso}
-          onChangeText={setCurso}
-          placeholder="Curso"
-        />
+          <Text style={styles.sectionSubTitle}>Nome</Text>
+          <TextInput
+            style={styles.input}
+            value={nome}
+            onChangeText={setNome}
+            placeholder="Nome"
+          />
 
-        <Text style={styles.sectionSubTitle}>Email cadastrado</Text>
-        <TextInput
-          style={styles.input}
-          value={email}
-          onChangeText={setEmail}
-          placeholder="Email cadastrado"
-        />
+          <Text style={styles.sectionSubTitle}>Curso</Text>
+          <TextInput
+            style={styles.input}
+            value={curso}
+            onChangeText={setCurso}
+            placeholder="Curso"
+          />
 
-        <Text style={styles.sectionSubTitle}>Matrícula</Text>
-        <TextInput
-          style={styles.input}
-          value={matricula}
-          onChangeText={setMatricula}
-          placeholder="Matrícula"
-        />
-      </View>
+          <Text style={styles.sectionSubTitle}>Email cadastrado</Text>
+          <TextInput
+            style={styles.input}
+            value={email}
+            onChangeText={setEmail}
+            placeholder="Email cadastrado"
+          />
 
-      {/* Gamificação */}
-      <View style={styles.section}>
-        <Text style={styles.sectionTitle}>Gamificação</Text>
-        <View style={styles.switchContainer}>
-          <Text style={{fontSize: 16 }}>Desejo participar do ranking</Text>
-          <Switch
-            value={participarRanking}
-            onValueChange={setParticiparRanking}
+          <Text style={styles.sectionSubTitle}>Matrícula</Text>
+          <TextInput
+            style={styles.input}
+            value={matricula}
+            onChangeText={setMatricula}
+            placeholder="Matrícula"
           />
         </View>
-      </View>
 
-      {/* Pontuação */}
-      <View style={styles.pontuacaoContainer}>
-        {participarRanking && (
-        <View style={styles.pontuacaoItem}>
-          <Text style={styles.pontuacaoNumero}>29°</Text>
-          <Text>Ranking</Text>
+        {/* Gamificação */}
+        <View style={styles.section}>
+          <Text style={styles.sectionTitle}>Gamificação</Text>
+          <View style={styles.switchContainer}>
+            <Text style={{ fontSize: 16 }}>Desejo participar do ranking</Text>
+            <Switch
+              value={participarRanking}
+              onValueChange={handleSwitchChange} 
+              trackColor={{ false: '#9C9C9C', true: '#4CAF50' }} // Cor do fundo do switch
+              thumbColor={participarRanking ? '#fff' : '#fff'} // Cor da bolinha do switch
+              style={{ transform: [{ scaleX: 1.5 }, { scaleY: 1.5 }] }} // Aumenta o tamanho do switch
+            />
+          </View>
         </View>
-        )}
-        <View style={styles.pontuacaoItem}>
-          <Text style={styles.pontuacaoNumero}>209</Text>
-          <Text>Respostas</Text>
-        </View>
-        <View style={styles.pontuacaoItem}>
-          <Text style={styles.pontuacaoNumero}>10</Text>
-          <Text>Perguntas</Text>
-        </View>
-      </View>
-      <View style={styles.pontuacaoContainer}>
-        {participarRanking && (
-        <View style={styles.pontuacaoItem}>
-          <Text style={styles.pontuacaoNumero}>4,75</Text>
-          <Text>Média</Text>
-        </View>
-        )}
-      </View>
 
-      {/* Conta */}
-      <View style={styles.section}>
-        <Text style={[styles.sectionTitle, { marginTop: 20 }]}>Conta</Text>
+        {/* Modal de Confirmação */}
+        <Modal
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={cancelSwitchChange}
+        >
+          <View style={styles.modalOverlay}>
+            <View style={styles.modalContainer}>
+              <Text style={styles.modalTitle}>Deseja desativar sua participação no ranking?</Text>
+              <Text style={styles.modalMessage}>Essa ação fará você perder sua posição atual</Text>
+              <View style={styles.modalButtons}>
+                <Button title="Cancelar" onPress={cancelSwitchChange} color="#FF6347" />
+                <Button title="Confirmar" onPress={confirmSwitchChange} color="#4CAF50" />
+              </View>
+            </View>
+          </View>
+        </Modal>
+
+        {/* Pontuação */}
+        <View style={styles.pontuacaoContainer}>
+          {participarRanking && (
+            <View style={styles.pontuacaoItem}>
+              <Text style={styles.pontuacaoNumero}>29°</Text>
+              <Text>Ranking</Text>
+            </View>
+          )}
+          <View style={styles.pontuacaoItem}>
+            <Text style={styles.pontuacaoNumero}>209</Text>
+            <Text>Respostas</Text>
+          </View>
+          <View style={styles.pontuacaoItem}>
+            <Text style={styles.pontuacaoNumero}>10</Text>
+            <Text>Perguntas</Text>
+          </View>
+        </View>
+        <View style={styles.pontuacaoContainer}>
+          {participarRanking && (
+            <View style={styles.pontuacaoItem}>
+              <Text style={styles.pontuacaoNumero}>4,75</Text>
+              <Text>Média</Text>
+            </View>
+          )}
+        </View>
+
+        {/* Conta */}
+        <View style={styles.section}>
+          <Text style={[styles.sectionTitle, { marginTop: 20 }]}>Conta</Text>
+
+          <TouchableOpacity>
+            <Text style={{ fontSize: 16, color: '#3b82f6' }}>Alterar senha</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity>
+            <Text style={styles.deleteLink}>Excluir conta</Text>
+          </TouchableOpacity>
+        </View>
 
         <TouchableOpacity>
-          <Text style={{fontSize: 16, color: '#3b82f6'}}>Alterar senha</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity>
-          <Text style={styles.deleteLink}>Excluir conta</Text>
+          <Text style={styles.logoutText}>Sair da conta</Text>
         </TouchableOpacity>
       </View>
-
-      <TouchableOpacity>
-        <Text style={styles.logoutText}>Sair da conta</Text>
-      </TouchableOpacity>
-    </View>
     </ScrollView>
   );
 };
@@ -168,9 +212,11 @@ const styles = StyleSheet.create({
   },
   pontuacaoItem: {
     alignItems: 'center',
+    justifyContent: 'center',
+    width: 120,
+    height: 100,
     backgroundColor: 'white',
-    margin: 10,
-    padding: 20,
+    margin: 5,
     borderRadius: 10,
   },
   pontuacaoNumero: {
@@ -189,6 +235,37 @@ const styles = StyleSheet.create({
     color: 'red',
     fontSize: 16,
     fontFamily: 'Raleway_700Bold',
+  },
+
+  // Estilos para o Modal
+  modalOverlay: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContainer: {
+    width: 300,
+    padding: 20,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  modalTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    textAlign: 'center',
+  },
+  modalMessage: {
+    fontSize: 16,
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  modalButtons: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
   },
 });
 
