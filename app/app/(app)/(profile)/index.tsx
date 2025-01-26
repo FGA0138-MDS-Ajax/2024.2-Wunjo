410243a22dcb6b905f46851425b9d1408d13a51c
arthurevg <arthurevangelista05@gmail.com>
2025-01-26 15:20:08 -0300

    .

--- app/app/(app)/(profile)/index.tsx
+++ app/app/(app)/(profile)/index.tsx
@@ -1,10 +1,36 @@
-import { Text } from "@/components/ui/text";
-import { View } from 'react-native';
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import ProfileHeader from '@/assets/images/profile-header';
 
 export default function Profile() {
+  const navigation = useNavigation();
+
   return (
     <View>
-      <Text>Perfil!</Text>
+      <View style={styles.headerbox}>
+        <ProfileHeader />
+      </View>
+      <View style={styles.container}>
+
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  headerbox: {
+    backgroundColor: '#173CAC',
+    height: 200,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  header: {
+    bottom: 0,
+    height: '100%',
+    transform: [{ scale: 2.3 }, { rotate: '180deg' }],
+  },
+});
